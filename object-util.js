ab380412c1ac0d17630e1a203bab00ec1d8f4f76
lizhe <lizhepro@gmail.com>
2012-07-07 00:54:05 +0000

    dropdown list data processing on the front-end update web interface file read using the parallel processing changed data to static json files removed wrench module

---
+++ object-util.js
@@ -0,0 +1,33 @@
+exports.extractObjectKeyPath = function(obj, key) {
+    var indexList = {};
+    var level = 0;
+    var path = [];
+    var iterObject = function(obj, key) {
+        var myKeys = Object.keys(obj);
+        for (var i = 0, k; k = myKeys[i]; i++) {
+            var v = obj[k];
+            if (typeof v === 'object') {
+                path[level] = k;
+                level++;
+                iterObject(v, key);
+            } else if (typeof v === 'string') {
+                if (k === key) {
+                    indexList[v] = path.slice(0, level).join('.');
+                }
+            }
+        }
+        level--;
+    };
+    iterObject(obj, key);
+
+    return indexList;
+};
+
+exports.getValueByPath = function(obj, path) {
+    var pathList = path.split('.');
+    var k;
+    while(k = pathList.shift()) {
+        obj = obj[k];
+    }
+    return obj;
+};
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,81 +1,89 @@
 $(document).ready(function() {
-  var indexList = null;
-  var searchInput = $('#searchinput');
-  var resultDiv = $('#result');
-  var autocomplete = searchInput.typeahead({
-    updater: function(item) {
-      var itemWithOutType = item.replace(/\[\w+\]/, '');
+    var indexList = null;
 
-      for(var key in indexList) {
-        var value = indexList[key];
-        if(value.textRaw == itemWithOutType) {
-          var outputHtml = '';
-          value.desc ? outputHtml += value.desc : null;
+    var apiList;
+    $.getJSON("/json/mydata.json", function(data) {
+        apiList = data;
+    });
 
-          resultDiv.html(outputHtml);
-        }
-      }
-      return itemWithOutType;
 
-    }
-  })
-  .on('keyup', function(ev){
+    var searchInput = $('#searchinput');
+    var resultDiv = $('#result');
+    var autocomplete = searchInput.typeahead({
+        updater: function(item) {
 
-    /* 			console.log(ev); */
-    ev.stopPropagation();
-    ev.preventDefault();
 
-    //filter out up/down, tab, enter, and escape keys
-    if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ){
-
-      var self = $(this);
-
-      //set typeahead source to empty
-      self.data('typeahead').source = [];
-
-      //active used so we aren't triggering duplicate keyup events
-      if( !self.data('active') && self.val().length > 0){
-
-        self.data('active', true);
-
-        //Do data request. Insert your own API logic here.
-        /*                     console.log($(this).val()); */
-
-        //Do data request. Insert your own API logic here.
-
-        $.getJSON("/query", {
-          q: self.val()
-        }, function(data) {
-          indexList = data;
-
-          //set this to true when your callback executes
-          self.data('active',true);
-
-          //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
-          var arr = [];
-          var i = 0;
-          for(var key in data) {
-            var value = data[key];
-            var typeString = '';
-            if(value.type) {
-              typeString = '[' + value.type + ']';
+            var itemPath;
+            var owner;
+            var myKeys = Object.keys(apiList);
+            for(var i=0, apiFileName; apiFileName=myKeys[i]; i++) {
+                var apiFileJSON = apiList[apiFileName];
+                var apiNameKeys = Object.keys(apiFileJSON);
+                for(var j=0, apiName; apiName=apiNameKeys[j]; j++) {
+                    if(apiName === item) {
+                        itemPath = apiFileJSON[apiName];
+                        owner = apiFileName;
+                        break;
+                    }
+                }
             }
-            arr[i++] = typeString + value.textRaw
-            console.log(value.textRaw);
-          }
 
-          //set your results into the typehead's source
-          self.data('typeahead').source = arr;
+            $.getJSON('/json/' + owner, function(data) {
+                var value = getValueByPath(data, itemPath);
+                resultDiv.html(value.desc);
+            });
 
-          //trigger keyup on the typeahead to make it search
-          self.trigger('keyup');
+            return item;
+        }
+    })
+    .on('keyup', function(ev) {
 
-          //All done, set to false to prepare for the next remote query.
-          self.data('active', false);
+        ev.stopPropagation();
+        ev.preventDefault();
 
-        });
+        if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ) {
+            var self = $(this);
 
-      }
-    }
-  });
+            self.data('typeahead').source = [];
+
+            if(!self.data('active') && self.val().length > 0){
+
+
+                if(self.val().length > 0){
+
+
+                    var rInput = new RegExp($(this).val(), 'i');
+                    var dropDownApiNames = [];
+                    var myKeys = Object.keys(apiList);
+                    for(var i=0, apiFileName; apiFileName=myKeys[i]; i++) {
+                        var apiFileJSON = apiList[apiFileName];
+                        var apiNameKeys = Object.keys(apiFileJSON);
+                        for(var j=0, apiName; apiName=apiNameKeys[j]; j++) {
+                            if(rInput.test(apiName)) {
+                                dropDownApiNames.push(apiName);
+                            }
+                        }
+                    }
+                    self.data('active', true);
+
+                    //set your results into the typehead's source
+                    self.data('typeahead').source = dropDownApiNames;
+
+                    //trigger keyup on the typeahead to make it search
+                    self.trigger('keyup');
+
+                    self.data('active', false);
+                }
+            }
+        }
+    });
 });
+
+var getValueByPath = function(obj, path) {
+    var pathList = path.split('.');
+    var k;
+    while(k = pathList.shift()) {
+        obj = obj[k];
+    }
+    return obj;
+};
