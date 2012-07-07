92826ceceac968f2fc05a7996c1d4b3e6f0469e9
lizhe <lizhepro@gmail.com>
2012-07-07 00:54:05 +0000

    下拉列表数据放到前端处理 更新了web界面样式 文件采用parallel同时进行处理 数据改为静态json文件

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,13 @@
+var objectUtil = require('../object-util');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-    res.render('index', { title: 'nodejsApi' });
+    res.render('index', { title: 'NodejsAPI' });
 };
 
 
-exports.query = function(req, res, arr) {
-  var input = req.query['q'];
-  var result = {};
-  var rInput = null;
-  try{
-    rInput = new RegExp(input, 'i');
-  } catch(err) {
-    console.log(err.toString());
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end();
-    return;
-  }
-  var i = 0;
-  for(var key in arr) {
-    if(rInput.test(key)) {
-      if(i++==8) break;
-      result[key] = arr[key];
-    }
-  }
+exports.query = function(req, res) {
 
-  i == 0 ? result = '' : null;
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(result));
 };
