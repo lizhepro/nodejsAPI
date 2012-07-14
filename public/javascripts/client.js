9af153fb632cef56689a25ae88c433e39494ea41
lizhe <lizhepro@gmail.com>
2012-07-14 00:29:24 +0000

    added navigation bar

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
       var itemName = apiObj.item;
       var itemPath;
       var myKeys = Object.keys(apiList);
+      //获取用户输入api名称对应的路径
       for(var i=0, apiFileName; apiFileName=myKeys[i]; i++) {
         if(itemPath) break;
         var apiFileJSON = apiList[apiFileName];
@@ -30,13 +31,21 @@ $(document).ready(function() {
         }
       }
 
+
+      //通过api的owner和path获取api对象
       $.getJSON('/json/' + nodeVersion
         + '/' + apiObj.owner, function(data) {
         var value = getValueByPath(data, itemPath);
-        console.log(value);
 
         apiSignatures.html(generateSignatureHtml(value));
         apiDescDiv.html(value.desc);
+
+        var parentPath = itemPath.split('.');
+        parentPath.pop();
+        var parent = getValueByPath(data, parentPath.join('.'));
+        updateClassNavigation(parent, itemName);
+
+
 
         $('code').parent('pre').addClass('prettyprint');
         prettyPrint();
@@ -87,80 +96,129 @@ $(document).ready(function() {
       }
     }
   });
-});
 
-var getValueByPath = function(obj, path) {
-  var pathList = path.split('.');
-  var k;
-  while(k = pathList.shift()) {
-    obj = obj[k];
-  }
-  return obj;
-};
-
-var getParamNames = function(params) {
-  var returnNameList = [];
-  for(var i=0, param; param=params[i]; i++) {
-    var paramName = param.name;
-    if(param.optional) {
-      paramName = '[' + param.name + ']';
+  /*
+  * 通过路径获取对象
+  */
+  var getValueByPath = function(obj, path) {
+    var pathList = path.split('.');
+    var k;
+    while(k = pathList.shift()) {
+      obj = obj[k];
     }
-    returnNameList.push(paramName);
-  }
-  return returnNameList.join(', ');
-};
+    return obj;
+  };
 
-var getParamInfoHtml = function(params) {
-  var paramInfoHtml = '';
-  for(var i=0, param; param=params[i]; i++) {
-    paramInfoHtml += '<li>';
-    var paramInfo = param.name;
-    if(param.desc) {
-      paramInfo += ' ' + param.desc;
+  /*
+  * 获取api的参数名称
+  */
+  var getParamNames = function(params) {
+    var returnNameList = [];
+    for(var i=0, param; param=params[i]; i++) {
+      var paramName = param.name;
+      if(param.optional) {
+        paramName = '[' + param.name + ']';
+      }
+      returnNameList.push(paramName);
     }
-    paramInfoHtml += paramInfo;
-    paramInfoHtml += '</li>';
-  }
-  return paramInfoHtml;
-};
+    return returnNameList.join(', ');
+  };
 
-var generateSignatureHtml = function(value) {
-  var signatureList = value.signatures;
-  var returnHtml = '';
-  if(!signatureList) return returnHtml;
+  /*
+  * 生成api参数信息html
+  */
+  var generateParamInfoHtml = function(params) {
+    var paramInfoHtml = '';
+    for(var i=0, param; param=params[i]; i++) {
+      paramInfoHtml += '<li>';
+      var paramInfo = param.name;
+      if(param.desc) {
+        paramInfo += ' ' + param.desc;
+      }
+      paramInfoHtml += paramInfo;
+      paramInfoHtml += '</li>';
+    }
+    return paramInfoHtml;
+  };
 
-  for(var i=0,signature; signature=signatureList[i]; i++) {
-    var signatureListKeys = Object.keys(signature);
-    var signatureDesc = signature['desc'];
-    var signatureParams = signature['params'];
-    var signatureReturn = signature['return'];
+  /*
+  * 生成signature html
+  */
+  var generateSignatureHtml = function(value) {
+    var signatureList = value.signatures;
+    var returnHtml = '';
+    if(!signatureList) return returnHtml;
 
-    if(signatureDesc) {
-      if(returnHtml.indexOf(signatureDesc) != -1) {
-        continue;
+    for(var i=0,signature; signature=signatureList[i]; i++) {
+      var signatureListKeys = Object.keys(signature);
+      var signatureDesc = signature['desc'];
+      var signatureParams = signature['params'];
+      var signatureReturn = signature['return'];
+
+      if(signatureDesc) {
+        if(returnHtml.indexOf(signatureDesc) != -1) {
+          continue;
+        }
+      }
+
+      if(value.type === 'class') {
+        returnHtml += '<h3>new ' + value.name
+        + '(' + getParamNames(signatureParams) + ')' + '</h3>';
+        returnHtml += '<ul>';
+        returnHtml += generateParamInfoHtml(signatureParams);
+      } else {
+        returnHtml += '<ul>';
+      }
+      if(signatureReturn) {
+        returnHtml += '<li>' +  signatureReturn.textRaw + '</li>';
+      }
+      returnHtml += '</ul>';
+
+      if(signatureDesc) {
+        returnHtml += signatureDesc;
       }
     }
+    return returnHtml;
+  };
 
-    if(value.type === 'class') {
-      returnHtml += '<h3>new ' + value.name
-      + '(' + getParamNames(signatureParams) + ')' + '</h3>';
-      returnHtml += '<ul>';
-      returnHtml += getParamInfoHtml(signatureParams);
-    } else {
-      returnHtml += '<ul>';
-    }
-    if(signatureReturn) {
-      returnHtml += '<li>' +  signatureReturn.textRaw + '</li>';
-    }
-    returnHtml += '</ul>';
-
-    if(signatureDesc) {
-      returnHtml += signatureDesc;
-    }
-  }
-  return returnHtml;
-};
-
-var escapeInput = function(text) {
+  /*
+  * 对输入的文本进行转义
+  */
+  var escapeInput = function(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+  };
+
+  var updateClassNavigation = function(parent, itemName) {
+    if(!parent) return false;
+    var frag = document.createDocumentFragment();
+    for(var i=0, child; child=parent[i]; i++) {
+      var $li = $('<li />');
+      var $a = $('<a />');
+      var textRaw = child.textRaw;
+      if(textRaw === itemName) {
+        $li.addClass('active');
+      }
+      var methodName = textRaw.replace(/\([^)]*?\)$/, '');
+      $a.text(methodName)
+        .prop('apiObj', child)
+        .on('click', function() {
+          var obj = $(this).prop('apiObj');
+
+          $(this).parent().siblings().removeClass('active')
+            .end()
+            .addClass('active');
+
+          searchInput.val(obj.textRaw);
+
+          apiSignatures.html(generateSignatureHtml(obj));
+          apiDescDiv.html(obj.desc);
+
+        });
+
+      $li.append($a);
+      frag.appendChild($li[0]);
+    }
+    $('#class-navigation ul').empty().append(frag);
+  };
+});
+
